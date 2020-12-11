88e4a776858471d8558297fe201672cadd8e6814
Roberto Andrés Carrasco Cavieres <roberto@Robertos-MacBook-Pro.local>
2020-12-11 17:18:39 -0300

    Agrego modulo para buscar un usuario en L.txt

---
+++ buscar.py
@@ -0,0 +1,16 @@
+import os
+
+#Busca un usuario usando grep, retorna verdadero si esta y sino falso
+def buscar(username):
+    #username = 'BfgHToYfSueoxPSgBHsToacJ2'
+    cmd = f"grep "+username+" L.txt" 
+    #cmd = f"grep ’{username}’ L.txt"
+    a = os.system(cmd)
+    if a == 0:
+        return True
+    else:
+        return False
+
+
+#Test
+#print(buscar('BfgHToYfSueoxPSgBHsToacJ'))
